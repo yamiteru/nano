4dac4f8e95c3b3b1ce74c57d45cc9914fbdeb424
Miroslav Vršecký <office@yamiteru.xyz>
2022-07-12 22:49:36 +0200

    Add prototype of struct

--- src/struct/index.ts
+++ src/struct/index.ts
@@ -1,32 +1,109 @@
-import { next, pull, value } from "../value";
+import {
+    stream,
+    next as streamNext,
+    onNext as streamOnNext,
+    error as streamError,
+    onError as streamOnError,
+    close as streamClose,
+    onClose as streamOnClose,
+} from "../stream";
+import {
+    value,
+    next as ValueNext,
+    pull as valuePull,
+    close as valueClose,
+} from "../value";
+import { STREAM } from "../value/_/constants";
+import {
+    TSubscriber,
+} from "../_/types";
 
 export function struct<T extends Record<any, any>>(v: T) {
+    let data = Object.create(v);
+
     const values: any = {};
     const proxy: any = {};
 
-    for(const k in v) {
+    const $stream = stream<[key: string, value: string]>(null, {
+        close: () => {
+            for(const k in values) {
+                valueClose(values[k]);
+            }
+        }
+    });
+
+    for(const k in data) {
+        const isFunction = typeof data[k] === "function";
+        const checkComputed = () => {
+            if(!values[k] && isFunction) {
+                values[k] = data[k](proxy);
+            }
+        };
+
         proxy[k] = (value: any) => {
+            checkComputed();
+
             const _$ = values[k];
 
-            if(value) {
+            if(!isFunction && value) {
                 if(_$) {
-                    next(_$, value);
+                    ValueNext(_$, value);
                 } else {
-                    v[k] = value;
+                    data[k] = value;
                 }
+
+                streamNext($stream, [k, value]);
             }
 
-            return _$ ? pull(values[k]): v[k];
+            return _$
+                ? valuePull(_$)
+                : data[k];
         };
 
         proxy[`$${k}`] = () => {
-            if(values[k] === undefined) {
-                values[k] = value(v[k]);
+            checkComputed();
+
+            if(!values[k]) {
+                values[k] = value(data[k]);
             }
 
             return values[k];
         };
     }
 
+    proxy[STREAM] = () => $stream;
+
     return proxy;
+}
+
+export function pull($struct: any) {
+    const res: any = {};
+
+    for(const k in $struct) {
+        if(k[0] !== "$") {
+            res[k] = $struct[k]();
+        }
+    }
+
+    return res;
+}
+
+export function close($struct: any) {
+    streamClose($struct[STREAM]);
+}
+
+export function onClose($struct: any, subsciber: TSubscriber<undefined>) {
+    return streamOnClose($struct[STREAM], subsciber);
+}
+
+export function error($struct: any, message: string) {
+    streamError($struct[STREAM], message);
+}
+
+export function onError($struct: any, subsciber: TSubscriber<string>) {
+    return streamOnError($struct[STREAM], subsciber);
+}
+
+export function onNext($struct: any, subsciber: TSubscriber<any>) {
+    return streamOnNext($struct[STREAM], subsciber);
 }
